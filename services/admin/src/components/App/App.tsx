d7a37ed0879a6f970783fc6019c01277a275ee69
Maks-yaf <maksim.yafimchyk@gamil.com>
2024-06-24 13:45:26 +0300

    First commit

---
+++ services/admin/src/components/App/App.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import {Link, Outlet} from "react-router-dom";
+
+export const App = () => {
+    return (
+        <div data-testid={"appDataTestId"}>
+            <h1>PAGE</h1>
+            <Link to={'/about'}>ABOUT</Link>
+            <br/>
+            <Link to={'/shop'}>SHOP</Link>
+            <Outlet/>
+        </div>
+    );
+};
---
+++ services/host/src/index.tsx
@@ -0,0 +1,22 @@
+import {createRoot} from "react-dom/client";
+import {App} from "./components/App/App";
+import React from "react"
+import {createBrowserRouter, RouterProvider} from "react-router-dom";
+
+const root = document.getElementById("root");
+
+if (!root) {
+    throw new Error('root not found')
+}
+
+const container = createRoot(root);
+
+const router = createBrowserRouter([
+    {
+        path: "/",
+        element: <App/>,
+        children: []
+    },
+]);
+
+container.render(<RouterProvider router={router}/>)
