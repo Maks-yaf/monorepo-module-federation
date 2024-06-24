e77f5b14bd6de30fa7c4ca3d906f8f02d9af63c7
Maks-yaf <maksim.yafimchyk@gamil.com>
2024-06-24 17:06:19 +0300

    Third commit

--- packages/shared/src/components/UserCard.tsx
+++ packages/shared/src/components/UserCard.tsx
@@ -4,7 +4,7 @@ const UserCard = ({username}: {username?:string}) => {
     return (
         <div style={{border: '3px solid green', padding: 20}}>
             <div>username: {username ?? 'user'}</div>
-            <div>password: 123</div>
+            <div>password: 1234</div>
         </div>
     );
 };
